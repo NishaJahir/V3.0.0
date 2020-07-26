d5e4218542b2160d8d963447097e8fe09bd77486
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-07-26 16:09:36 +0530

    Update CreateAndUpdatePaymentMethodName.php

--- src/Migrations/CreateAndUpdatePaymentMethodName.php
+++ src/Migrations/CreateAndUpdatePaymentMethodName.php
@@ -59,8 +59,8 @@ class CreateAndUpdatePaymentMethodName
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_INVOICE', 'Novalnet Invoice');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_CC', 'Novalnet Credit Card');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_SEPA', 'Novalnet Direct Debit SEPA');
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_PAYPAL', 'Novalnet PayPal');
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_INSTALLMENT_INVOICE', 'Novalnet Installment Invoice')
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_INSTALLMENT_INVOICE', 'Novalnet Installment Invoice');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_PAYPAL', 'Novalnet PayPal')
     }
 
 
