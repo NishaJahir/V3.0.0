70e11052307c4ce2c3e85c9b3057030b20760778
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-07-30 12:34:21 +0530

    Update NovalnetInstalmentbyInvoicePaymentMethod.php

--- src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
+++ src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
@@ -94,9 +94,9 @@ class NovalnetInstalmentbyInvoicePaymentMethod extends PaymentMethodBaseService
 	    if (!empty($maximum_amount) && is_numeric($maximum_amount)) {
 		$active_payment_maximum_amount = $this->paymentService->getMaxBasketAmount($this->basket, $maximum_amount);
 		}
+	     $paymentConditionMet =  $this->paymentService->getPaymentDisplayCondition($this->basket, 'novalnet_instalment_invoice');
 	    
-	    
-        return (bool)($this->paymentHelper->paymentActive() && $active_payment_allowed_country && $active_payment_minimum_amount && $active_payment_maximum_amount);
+        return (bool)($this->paymentHelper->paymentActive() && $active_payment_allowed_country && $active_payment_minimum_amount && $active_payment_maximum_amount && $paymentConditionMet);
         } 
         return false;
     
