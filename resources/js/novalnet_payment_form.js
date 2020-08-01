b8857b8afa2944707b1e2ab50566b3ff35b871ed
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-08-01 15:19:29 +0530

    Update novalnet_payment_form.js

--- resources/js/novalnet_payment_form.js
+++ resources/js/novalnet_payment_form.js
@@ -8,13 +8,13 @@ function initIframe()
     var request = {
         callBack: 'createElements',
         customStyle: {
-            labelStyle: $('#nn_cc_standard_style_label').val(),
-            inputStyle: $('#nn_cc_standard_style_input').val(),
-            styleText: $('#nn_cc_standard_style_css').val(),
+            labelStyle: $('#nnCcStandardStyleLabel').val(),
+            inputStyle: $('#nnCcStandardStyleInput').val(),
+            styleText: $('#nnCcStandardStyleCss').val(),
             }
     };
 
-    var iframe = $('#nn_iframe')[0];
+    var iframe = $('#nnIframe')[0];
     iframeWindow = iframe.contentWindow ? iframe.contentWindow : iframe.contentDocument.defaultView;
     targetOrigin = 'https://secure.novalnet.de';
     iframeWindow.postMessage(JSON.stringify(request), targetOrigin);
@@ -22,9 +22,9 @@ function initIframe()
 
 function getHash(e)
 {   
-    $('#novalnet_form_btn').attr('disabled',true);
+    $('#novalnetFormBtn').attr('disabled',true);
     
-    if($('#nn_pan_hash').val().trim() == '') {
+    if($('#nnPanHash').val().trim() == '') {
         alert('yes');
         e.preventDefault();
         e.stopImmediatePropagation();
@@ -43,17 +43,12 @@ function getHash(e)
 
 function reSize()
 {
-    if ($('#nn_iframe').length > 0) {
-        var iframe = $('#nn_iframe')[0];
+    if ($('#nnIframe').length > 0) {
+        var iframe = $('#nnIframe')[0];
         iframeWindow = iframe.contentWindow ? iframe.contentWindow : iframe.contentDocument.defaultView;
         targetOrigin = 'https://secure.novalnet.de/';
         iframeWindow.postMessage(JSON.stringify({'callBack' : 'getHeight'}), targetOrigin);
     }
-}
-
-function novalnetCcIframe()
-{
-    $('#cc_loading').hide();
 }
 
 window.addEventListener(
@@ -63,17 +58,17 @@ window.addEventListener(
     if (e.origin === 'https://secure.novalnet.de') {
         if (data['callBack'] == 'getHash') {
             if (data['error_message'] != undefined) {
-                $('#novalnet_form_btn').attr('disabled',false); 
+                $('#novalnetFormBtn').attr('disabled',false); 
                 alert($('<textarea />').html(data['error_message']).text());
             } else {
-        $('#nn_pan_hash').val(data['hash']);
-                $('#nn_unique_id').val(data['unique_id']);
-                $('#novalnet_form').submit();
+        $('#nnPanHash').val(data['hash']);
+                $('#nnUniqueId').val(data['unique_id']);
+                $('#novalnetForm').submit();
             }
         }
 
         if (data['callBack'] == 'getHeight') {
-            $('#nn_iframe').attr('height', data['contentHeight']);
+            $('#nnIframe').attr('height', data['contentHeight']);
         }
     }
     }, false
@@ -88,19 +83,19 @@ $(document).ready( function () {
     }
     
     if(paymentName == 'NOVALNET_SEPA') {
-        $('#nn_sepa_iban').on('input',function ( event ) {
+        $('#nnSepaIban').on('input',function ( event ) {
         let iban = $(this).val().replace( /[^a-zA-Z0-9]+/g, "" ).replace( /\s+/g, "" );
             $(this).val(iban);      
         });
     
-        $('#nn_sepa_cardholder').keypress(function (event) {
+        $('#nnSepaCardholder').keypress(function (event) {
          var keycode = ( 'which' in event ) ? event.which : event.keyCode,
          reg     = /[^0-9\[\]\/\\#,+@!^()$~%'"=:;<>{}\_\|*?`]/g;
          return ( reg.test( String.fromCharCode( keycode ) ) || 0 === keycode || 8 === keycode );
          });
 
-        $('#novalnet_form').on('submit',function(){
-          $('#novalnet_form_btn').attr('disabled',true);      
+        $('#novalnetForm').on('submit',function(){
+          $('#novalnetFormBtn').attr('disabled',true);      
         });
     }
     
