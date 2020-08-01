7c205f1bd92006e93134f1545ac535fb6554619d
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-08-01 14:41:27 +0530

    Update and rename novalnet_cc.js to novalnet_payment_form.js

--- resources/js/novalnet_payment_form.js
+++ resources/js/novalnet_payment_form.js
@@ -1,6 +1,7 @@
 var $ = jQuery.noConflict();
 var nnButton, nnIfrmButton, iframeWindow, targetOrigin;
 nnButton = nnIfrmButton = iframeWindow = targetOrigin = false;
+var paymentName = $('#paymentKey').val();
 
 function initIframe()
 {
@@ -78,12 +79,29 @@ window.addEventListener(
     }, false
 );
 
-$(document).ready(
-    function () {
-    $(window).resize(
-        function() {
-        reSize();
-        }
-    );
+$(document).ready( function () {
+    
+    if(paymentName == 'NOVALNET_CC') {
+        $(window).resize( function() {
+            reSize();
+        });
     }
-);
+    
+    if(paymentName == 'NOVALNET_SEPA') {
+        $('#nn_sepa_iban').on('input',function ( event ) {
+        let iban = $(this).val().replace( /[^a-zA-Z0-9]+/g, "" ).replace( /\s+/g, "" );
+            $(this).val(iban);      
+        });
+    
+        $('#nn_sepa_cardholder').keypress(function (event) {
+         var keycode = ( 'which' in event ) ? event.which : event.keyCode,
+         reg     = /[^0-9\[\]\/\\#,+@!^()$~%'"=:;<>{}\_\|*?`]/g;
+         return ( reg.test( String.fromCharCode( keycode ) ) || 0 === keycode || 8 === keycode );
+         });
+
+        $('#novalnet_form').on('submit',function(){
+          $('#novalnet_form_btn').attr('disabled',true);      
+        });
+    }
+    
+});
