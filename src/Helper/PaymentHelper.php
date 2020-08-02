14f5920c8dddc8c75df4e16b41dc0c5d8bff0e6c
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-08-02 20:14:10 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -638,5 +638,9 @@ class PaymentHelper
         $this->assignPlentyPaymentToPlentyOrder($paymentObj, (int)$paymentData['child_order_id']);
     }
     
+    public function logger($key, $value) {
+        $this->getLogger(__METHOD__)->error($key, $value);
+    }
+    
    
 }
